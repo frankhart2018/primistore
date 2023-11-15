cda11c9fe395c6d0e4f861a071cea1c833b211ee
frankhart2018 <choudhury.si@northeastern.edu>
2023-11-14 19:33:50 -0600

    Remove PMS path from passwords table

--- app/backend/controllers/primistore/primistore-schema.js
+++ app/backend/controllers/primistore/primistore-schema.js
@@ -5,7 +5,6 @@ const schema = new mongoose.Schema(
     aes_key: String,
     aes_iv: String,
     pass_uid: String,
-    pms_path: { type: String, default: "" },
     charset_path: String,
   },
   {
