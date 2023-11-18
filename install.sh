f6e5d3239c5aafdc2eb3d5226ea21397aa9a0767
frankhart2018 <choudhury.si@northeastern.edu>
2023-11-18 00:12:01 -0600

    Fix docker compose download url in install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://github.com/frankhart2018/primistore/blob/master/app/docker-compose.prod.yml -O docker-compose.yml
+wget https://raw.githubusercontent.com/frankhart2018/primistore/master/app/docker-compose.prod.yml -O docker-compose.yml
 mkdir -p config
 LOCAL_DIR=config IP=$1 docker-compose up
