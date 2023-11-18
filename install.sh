161c1b620d21a9a997e2e20652d2386223e448bb
frankhart2018 <choudhury.si@northeastern.edu>
2023-11-18 00:02:18 -0600

    Add a basic installer script

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/frankhart2018/primistore/master/app/docker-compose.yml?token=GHSAT0AAAAAACIAMX6A44VURWYDO45RVSRUZKYKMPQ -O docker-compose.yml
+docker-compose up
