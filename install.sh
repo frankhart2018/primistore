75a99540e7accdbd50c7cbca800577b43dcabc22
frankhart2018 <choudhury.si@northeastern.edu>
2023-11-18 00:14:31 -0600

    Remove install scripts

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/frankhart2018/primistore/master/app/docker-compose.prod.yml -O docker-compose.yml
-mkdir -p config
-LOCAL_DIR=config IP=$1 docker-compose up
