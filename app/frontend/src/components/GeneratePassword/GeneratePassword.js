90b7f89c05853cea36ac323b22003051fd2aa6ab
frankhart2018 <choudhury.si@northeastern.edu>
2023-11-24 19:43:38 -0600

    Add sliders, input box for length and basic checks

--- app/frontend/src/components/GeneratePassword/GeneratePassword.js
+++ app/frontend/src/components/GeneratePassword/GeneratePassword.js
@@ -1,11 +1,88 @@
-import React from "react";
+import { useState } from "react";
 import NavBar from "../NavBar/NavBar";
+import Slider from "../Slider/Slider";
 
 const GeneratePassword = () => {
+  const [sliderValues, setSliderValues] = useState({ 0: 100 });
+  const [generatedPassword, setGeneratedPassword] = useState("");
+  const [passwordLength, setPasswordLength] = useState(0);
+
+  const childValueUpdateCallback = (count, value) => {
+    setSliderValues({ ...sliderValues, [count]: value });
+  };
+
+  const generatePasswordHandler = () => {
+    setGeneratedPassword("");
+    let values = Object.values(sliderValues);
+    const valuesSum = values.reduce((acc, val) => acc + parseInt(val), 0);
+
+    let errVals = [];
+    if (valuesSum !== 100) {
+      errVals.push(
+        `The percentages must add up to 100%, current sum is ${valuesSum}%`
+      );
+    }
+
+    if (passwordLength === 0) {
+      errVals.push("The password length should be greater than 0");
+    }
+
+    if (errVals.length > 0) {
+      alert(errVals.join("\n"));
+    } else {
+      setGeneratedPassword("helloWorld");
+    }
+  };
+
   return (
     <div>
       <NavBar />
-      <span>GeneratePassword</span>
+      <br />
+      <div>
+        <div>
+          <label for="password-length">Password length: </label>
+          <input
+            type="number"
+            id="password-length"
+            value={passwordLength}
+            onChange={(e) => setPasswordLength(parseInt(e.target.value))}
+          />
+        </div>
+
+        <Slider
+          initialValue={100}
+          label="Alphabets (uppercase + lowercase) percent: "
+          count={0}
+          parentUpdateCallback={childValueUpdateCallback}
+        />
+
+        <Slider
+          initialValue={0}
+          label="Number percent: "
+          count={1}
+          parentUpdateCallback={childValueUpdateCallback}
+        />
+
+        <Slider
+          initialValue={0}
+          label="Special characters percent: "
+          count={2}
+          parentUpdateCallback={childValueUpdateCallback}
+        />
+
+        <div>
+          <button onClick={generatePasswordHandler}>Generate Password</button>
+        </div>
+
+        {generatedPassword.length > 0 ? (
+          <div>
+            <strong>The generated password is: </strong>
+            {generatedPassword}
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 };
---
+++ app/frontend/src/components/Slider/Slider.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const Slider = ({ initialValue, label, count, parentUpdateCallback }) => {
+  const [sliderValue, setSliderValue] = useState(initialValue);
+
+  const onUpdateSlider = (e) => {
+    const thisVal = e.target.value;
+    setSliderValue(thisVal);
+    parentUpdateCallback(count, thisVal);
+  };
+
+  return (
+    <div>
+      <label for={`slider-${count}`}>{label}</label>
+      <input
+        type="range"
+        min="1"
+        max="100"
+        id={`slider-${count}`}
+        value={sliderValue}
+        onChange={onUpdateSlider}
+      />
+      <span>{sliderValue}%</span>
+    </div>
+  );
+};
+
+export default Slider;
