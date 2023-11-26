4689a452d1ad7d18c1493fe78393efa326c65455
frankhart2018 <choudhury.si@northeastern.edu>
2023-11-26 17:35:35 -0600

    Bye bye experiments

--- experiments/aes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-AES_KEY=$(openssl rand -hex 32)
-AES_IV=$(openssl rand -hex 16)
-OUTPUT=`echo "hello world" | openssl aes-256-cbc -e -a -K $AES_KEY -iv $AES_IV`
-echo $OUTPUT
-echo $OUTPUT | openssl aes-256-cbc -d -a -K $AES_KEY -iv $AES_IV
