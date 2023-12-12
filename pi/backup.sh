095227944784521a602d94e35599bff8dca2c4bd
frankhart2018 <choudhury.si@northeastern.edu>
2023-12-11 19:08:49 -0600

    Add backup script for pi

---
+++ pi/backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+MONGO_CONTAINER_COUNTS=$(sudo docker ps -q --filter "ancestor=mongo:bionic" | wc -l)
+if [ "$MONGO_CONTAINER_COUNTS" -eq "1" ]; then
+    CURRENT_TIME=$(date +%s)
+    BACKUP_DIR=primistore-$CURRENT_TIME
+    BACKUP_TARBALL_PATH=$BACKUP_DIR.tar.gz
+    PRIMISTORE_DIR=.primistore
+    PRIMISTORE_TARBALL_PATH=primistore-backup-$CURRENT_TIME.tar.gz
+
+    cd ~/
+    MONGO_CONTAINER_ID=$(sudo docker ps -q --filter "ancestor=mongo:bionic" | head -n 1)
+    sudo docker exec -it $MONGO_CONTAINER_ID mongodump --host localhost --db primistore
+    sudo docker cp $MONGO_CONTAINER_ID:/dump .
+    sudo tar czf mongodump.tar.gz dump
+    sudo rm -rf dump
+    sudo docker exec -it $MONGO_CONTAINER_ID rm -rf /dump
+    sudo mkdir $BACKUP_DIR
+    sudo mv mongodump.tar.gz $BACKUP_DIR
+    sudo tar czf $PRIMISTORE_TARBALL_PATH $PRIMISTORE_DIR
+    sudo mv $PRIMISTORE_TARBALL_PATH $BACKUP_DIR
+    sudo tar czf $BACKUP_TARBALL_PATH $BACKUP_DIR
+    sudo rm -rf $BACKUP_DIR
+else
+    echo "More than one containers running MongoDB, please check your docker setup"
+fi
