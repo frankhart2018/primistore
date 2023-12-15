57d12227f9d089c9840005d09f83c5c3d25d454d
frankhart2018 <choudhury.si@northeastern.edu>
2023-12-14 22:34:06 -0600

    Move command running to the function

--- pi/primistore-startup.sh
+++ pi/primistore-startup.sh
@@ -4,7 +4,8 @@
 # up the apps
 
 check_command_installed() {
-	if [ -z "$1" ]; then
+    OUTPUT=$(which $1)
+	if [ -z "$OUTPUT" ]; then
 		echo "Please install $2, use 'sudo apt install $2 -y'"
         return 1
 	fi
@@ -12,15 +13,12 @@ check_command_installed() {
     return 0
 }
 
-IFCONFIG_OUTPUT=$(which ifconfig)
 TOTAL=0
-check_command_installed "$IFCONFIG_OUTPUT" "net-tools"
+check_command_installed "ifconfig" "net-tools"
 TOTAL=$((TOTAL + $?))
-DOCKER_OUTPUT=$(which docker)
-check_command_installed "$DOCKER_OUTPUT" "docker.io"
+check_command_installed "docker" "docker.io"
 TOTAL=$((TOTAL + $?))
-DOCKER_COMPOSE_OUTPUT=$(which docker-compose)
-check_command_installed "$DOCKER_COMPOSE_OUTPUT" "docker-compose"
+check_command_installed "docker-compose" "docker-compose"
 TOTAL=$((TOTAL + $?))
 if [ $TOTAL -gt 0 ]; then
 	echo "Some errors exist"
