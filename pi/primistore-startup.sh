822420e4147819b808ccb93246cc9e8dc7da1d44
frankhart2018 <choudhury.si@northeastern.edu>
2023-12-16 19:08:42 -0600

    Create output file if not exists

--- pi/primistore-startup.sh
+++ pi/primistore-startup.sh
@@ -33,5 +33,6 @@ DOCKER_COMPOSE_PATH=$HOME/docker-compose-prod-pi.yml
 
 mkfifo $PIPE_PATH
 python3 execute_pipe.py &
+touch $PIPE_OUTPUT_PATH
 
 sudo IP=$IP LOCAL_DIR=$LOCAL_DIR PIPE_PATH=$PIPE_PATH PIPE_OUTPUT_FILE=$PIPE_OUTPUT_PATH docker-compose -f $DOCKER_COMPOSE_PATH up -d
