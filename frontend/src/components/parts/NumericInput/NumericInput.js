5e0666520abc5efc58bd0ee0431171951dec3494
frankhart2018 <choudhury.si@northeastern.edu>
2023-12-16 19:42:21 -0600

    Replace slider with numeric input in generate password

---
+++ frontend/src/components/parts/NumericInput/NumericInput.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const NumericInput = ({ initialValue, label, count, parentUpdateCallback }) => {
+  const [numericValue, setNumericValue] = useState(initialValue);
+
+  const onUpdateValue = (e) => {
+    const thisVal = e.target.value;
+    setNumericValue(thisVal);
+    parentUpdateCallback(count, thisVal, "numeric");
+  };
+
+  return (
+    <tr className="border-none">
+      <td className="border-none py-2 w-fit">
+        <label className="capitalize" htmlFor={`numeric-${count}`}>
+          {label}
+        </label>
+      </td>
+      <td className="border-none">
+        <input
+          id={`numeric-${count}`}
+          type="number"
+          value={numericValue}
+          onChange={onUpdateValue}
+          className="bg-transparent border border-gray-950 text-gray-900 text-sm rounded-lg w-4/5 p-2.5 mb-2"
+        ></input>
+      </td>
+    </tr>
+  );
+};
+
+export default NumericInput;
