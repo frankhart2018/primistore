9a6dae2958f6df189a62fdac87d2e158f27304f1
frankhart2018 <choudhury.si@northeastern.edu>
2023-12-23 17:29:27 -0600

    Move tarball and docker exec into functions in backup script

--- pi/backup.sh
+++ pi/backup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+tarball() {
+    sudo tar czf $1 $2
+}
+
+docker_exec() {
+    sudo docker exec -it $1 $2
+}
+
 MONGO_CONTAINER_COUNTS=$(sudo docker ps -q --filter "ancestor=mongo:bionic" | wc -l)
 if [ "$MONGO_CONTAINER_COUNTS" -eq "1" ]; then
     CURRENT_TIME=$(date +%s)
@@ -10,16 +18,16 @@ if [ "$MONGO_CONTAINER_COUNTS" -eq "1" ]; then
 
     cd ~/
     MONGO_CONTAINER_ID=$(sudo docker ps -q --filter "ancestor=mongo:bionic" | head -n 1)
-    sudo docker exec -it $MONGO_CONTAINER_ID mongodump --host localhost --db primistore --quiet 
+    docker_exec $MONGO_CONTAINER_ID "mongodump --host localhost --db primistore --quiet"
     sudo docker cp $MONGO_CONTAINER_ID:/dump . > /dev/null 2>&1
-    sudo tar czf mongodump.tar.gz dump
+    tarball mongodump.tar.gz dump
     sudo rm -rf dump
-    sudo docker exec -it $MONGO_CONTAINER_ID rm -rf /dump
+    docker_exec $MONGO_CONTAINER_ID "rm -rf /dump"
     sudo mkdir $BACKUP_DIR
     sudo mv mongodump.tar.gz $BACKUP_DIR
-    sudo tar czf $PRIMISTORE_TARBALL_PATH $PRIMISTORE_DIR
+    tarball $PRIMISTORE_TARBALL_PATH $PRIMISTORE_DIR
     sudo mv $PRIMISTORE_TARBALL_PATH $BACKUP_DIR
-    sudo tar czf $BACKUP_TARBALL_PATH $BACKUP_DIR
+    tarball $BACKUP_TARBALL_PATH $BACKUP_DIR
     sudo rm -rf $BACKUP_DIR
     echo $BACKUP_TARBALL_PATH
 else
